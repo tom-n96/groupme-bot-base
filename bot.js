0c0f410cce1fdaba9b99798867e7e12fd2c6bda5
tom-n96 <areptom@yahoo.com>
2022-03-31 23:53:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -470,37 +470,4 @@ function postMessage(res) {
 	botResponse = "";
 }
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-
-}
-
-
-exports.respond = respond;
 
